e7f5acf79d54cf7b23c78b9e5e61ff5bc4f9e34e
Gautier <gautier.blandin@gmail.com>
2024-09-08 21:04:18 +0200

    libs: update centered layout

--- libs/web/shared/ui/src/lib/centered-layout.tsx
+++ libs/web/shared/ui/src/lib/centered-layout.tsx
@@ -7,7 +7,7 @@ interface CenteredLayoutProps {
 export const CenteredLayout: React.FC<CenteredLayoutProps> = ({ children }) => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-4xl">{children}</div>
+      <div className="w-fit max-w-4xl">{children}</div>
     </div>
   );
 };
