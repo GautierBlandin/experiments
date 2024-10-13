0049399773bb7f07f3e131f64432ba79faa6084f
Gautier <gautier.blandin@gmail.com>
2024-10-13 23:35:50 +0200

    experiment: remove useless files

--- libs/math/shuffle/src/lib/math-shuffle.spec.ts
+++
@@ -1,7 +0,0 @@
-import { mathShuffle } from './math-shuffle';
-
-describe('mathShuffle', () => {
-  it('should work', () => {
-    expect(mathShuffle()).toEqual('math-shuffle');
-  });
-});
--- libs/math/shuffle/src/lib/math-shuffle.ts
+++
@@ -1,3 +0,0 @@
-export function mathShuffle(): string {
-  return 'math-shuffle';
-}
